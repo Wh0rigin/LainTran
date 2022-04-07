10da59cb87425bc227214695f01a695ecb2f57f4
Lightsake-Sensei <805935229@qq.com>
2022-04-07 13:14:11 +0800

    Features edit

--- app/src/main/java/com/example/javacolorpic/MainActivity.java
+++ app/src/main/java/com/example/javacolorpic/MainActivity.java
@@ -2,18 +2,27 @@ package com.example.javacolorpic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnFocusChangeListener {
 
-    SeekBar sk_red,sk_green,sk_blue;
-    ImageView img;
-    TextView tv_red,tv_green,tv_blue,tv_hex;
+    private SeekBar sk_red,sk_green,sk_blue;
+    private ImageView img;
+    private EditText et_red,et_green,et_blue;
+    private TextView tv_hex;
+    private TextWatcher redWatcher,blueWatcher,greenWatcher;
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -59,9 +68,17 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         setContentView(R.layout.activity_main);
         init();
 
-      sk_red.setOnSeekBarChangeListener(this);
-      sk_blue.setOnSeekBarChangeListener(this);
-      sk_green.setOnSeekBarChangeListener(this);
+        sk_red.setOnSeekBarChangeListener(this);
+        sk_blue.setOnSeekBarChangeListener(this);
+        sk_green.setOnSeekBarChangeListener(this);
+
+        et_red.setOnFocusChangeListener(this);
+        et_blue.setOnFocusChangeListener(this);
+        et_green.setOnFocusChangeListener(this);
+
+        et_red.addTextChangedListener(redWatcher);
+        et_blue.addTextChangedListener(blueWatcher);
+        et_green.addTextChangedListener(greenWatcher);
     }
 
     private void init(){
@@ -69,10 +86,88 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         sk_green = findViewById(R.id.green_bar);
         sk_blue = findViewById(R.id.blue_bar);
         img = findViewById(R.id.img);
-        tv_red = findViewById(R.id.red);
-        tv_green = findViewById(R.id.green);
-        tv_blue = findViewById(R.id.blue);
+        et_red = findViewById(R.id.red);
+        et_green = findViewById(R.id.green);
+        et_blue = findViewById(R.id.blue);
         tv_hex = findViewById(R.id.hex);
+
+        redWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try{
+                    if(s.equals("")){
+                        sk_red.setProgress(0);
+                    }else{
+                        sk_red.setProgress(Integer.parseInt(s.toString()));
+                        et_red.setSelection(s.length());
+                    }
+                }catch (Exception e){
+                    // Toast.makeText(MainActivity.this,"请输入正确的数字",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        blueWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try{
+                    if(s.equals("")){
+                        sk_blue.setProgress(0);
+                    }else{
+                        sk_blue.setProgress(Integer.parseInt(s.toString()));
+                        et_blue.setSelection(s.length());
+                    }
+                }catch (Exception e){
+                    // Toast.makeText(MainActivity.this,"请输入正确的数字",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        greenWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try{
+                    if(s.equals("")){
+                        sk_green.setProgress(0);
+                    }else{
+                        sk_green.setProgress(Integer.parseInt(s.toString()));
+                        et_green.setSelection(s.length());
+                    }
+                }catch (Exception e){
+                    // Toast.makeText(MainActivity.this,"请输入正确的数字",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
     }
 
     private void changeColor(){
@@ -94,9 +189,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
                         :(Integer.toHexString(0xFF & sk_blue.getProgress()))
                 );
         tv_hex.setText(hex.toUpperCase());
-        tv_red.setText(sk_red.getProgress()+"");
-        tv_green.setText(sk_green.getProgress()+"");
-        tv_blue.setText(sk_blue.getProgress()+"");
+        et_red.setText(sk_red.getProgress()+"");
+        et_green.setText(sk_green.getProgress()+"");
+        et_blue.setText(sk_blue.getProgress()+"");
     }
 
     @Override
@@ -112,5 +207,48 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
 
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if(ev.getAction() ==  MotionEvent.ACTION_DOWN){
+            View v = getCurrentFocus();
+            if(!(v instanceof EditText)){
+                hideKeyboard();
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private void hideKeyboard(){
+        et_red.clearFocus();
+        et_blue.clearFocus();
+        et_green.clearFocus();
+        InputMethodManager imm = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getWindow().getDecorView().getWindowToken(),0);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if(v instanceof EditText && hasFocus){
+            EditText et = (EditText) v;
+            et.setText("");
+        }
+        else if(v instanceof EditText && !hasFocus && ((EditText) v).getText().toString().equals("")){
+            switch (v.getId()){
+                case R.id.red:
+                    sk_red.setProgress(0);
+                    ((EditText) v).setText("0");
+                    break;
+                case R.id.blue:
+                    ((EditText) v).setText("0");
+                    sk_blue.setProgress(0);
+                    break;
+                case R.id.green:
+                    ((EditText) v).setText("0");
+                    sk_green.setProgress(0);
+                    break;
+            }
+        }
     }
 }
